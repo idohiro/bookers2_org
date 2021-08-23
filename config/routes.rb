8f9db5e1de9387393f179159a1f6742a7bdef5a9
idohiro <idohiro0509@gmail.com>
2021-08-23 12:30:14 +0000

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+    root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :book,only: [:new, :create, :index, :show, :destroy] do
-    resources :home,only: [:index,:about]
+    resources :homes,only: [:index,:about]
     resources :users, only: [:show, :edit, :update]
   end
-end
+ end
