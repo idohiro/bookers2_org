47fbd79d65714469f64ee2bd11bfb8d00606f1b8
idohiro <idohiro0509@gmail.com>
2021-08-24 14:51:52 +0000

    createの部分

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.new(book_params)
+    book.save
+    # 以下の行を修正
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    # @books = Book.find(params[:id])
   end
 
   def destroy
   end
 
+  private
+  def book_params
+    params.require(:book).permit(:title,  :body)
+  end
 end
